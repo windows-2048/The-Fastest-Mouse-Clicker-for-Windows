f97b7de06e2c89492df50786b910fe2b30ab1420
windows-2048 <mashanovedad@gmail.com>
2019-03-14 09:54:44 -0400

    Prepare to Version 2.1.5.0 that coming soon...

--- main.cpp
+++ main.cpp
@@ -1059,7 +1059,7 @@ LRESULT CALLBACK winCallBack(HWND hWin, UINT msg, WPARAM wp, LPARAM lp)
 			}
 			break;
 		case HELP_BTN:
-			MessageBox(hWnd, "The Fastest Mouse Clicker for Windows 2.1.4.0 (Random Clicking; Different Keys For Toggle Mode; Window Always Top)."
+			MessageBox(hWnd, "The Fastest Mouse Clicker for Windows 2.1.5.0 (Random Clicking; Different Keys For Toggle Mode; Window Always Top)."
 				"\n\nYOU CAN START THE AUTO-CLICKING AT ANY MOMENT BY PRESSING THE <trigger key> (13 = Enter). Reading the entire Help is optional."
 				"\n\nTHE FIELDS YOU CAN NOT MODIFY."
 				"\n<clicking status> or <random clicking status>, the topmost text field, is either getting 'idle' or 'clicking'."
