8eea64f68c46014bfcd8e0c9d7b4e4b43924ea15
windows-2028 <mashanovedad@gmail.com>
2025-11-15 02:17:19 -0800

    add source code for TheFastestMouseClicker_v3, TheFastestMouseClicker_classic

--- build_by_gcc32.sh
+++
@@ -1,5 +0,0 @@
-# Copyright (c) 2016-2023 by [Open Source Developer Masha Novedad](https://windows-2048.github.io)
-
-rm -f TheFastestMouseClicker.exe
-gcc -static -O2 -Wl,--subsystem,windows main.cpp manifest.res -lstdc++ -lgdi32 -o TheFastestMouseClicker.exe
-strip -s TheFastestMouseClicker.exe
