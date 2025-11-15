19ac2fb7098517923a045ac2341394be37381842
windows-2028 <mashanovedad@gmail.com>
2025-11-15 01:56:32 -0800

    Intermediate commit - rm obsolete stuff

--- TheFastestMouseClicker_classic/build_by_gcc32.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-rm -f TheFastestMouseClickerClassic.exe
-/mingw32/bin/windres --input manifest.rc --output manifest.res --output-format=coff
-/mingw32/bin/gcc -static -O0 -Wl,--subsystem,windows main-1993.cpp manifest.res -lstdc++ -lgdi32 -o TheFastestMouseClickerClassic.exe
-/mingw32/bin/strip -s TheFastestMouseClickerClassic.exe
